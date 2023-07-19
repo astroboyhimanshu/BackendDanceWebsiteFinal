32efebd89c0932b318e70df34a6d90b823a0e287
Himanshu <78425898+astroboyhimanshu@users.noreply.github.com>
2023-07-19 17:56:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+
+// getting-started.js
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+} //connecting database
+
+
+
+
+const port = 8000;
+
+
+// Define Mongoose Schema 
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String
+
+  });
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+app.get('/home', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/about', (req, res)=>{
+    const params = {}
+    res.status(200).render('about.pug', params);
+})
+
+app.get('/services', (req, res)=>{
+    const params = {}
+    res.status(200).render('services.pug', params);
+})
+
+app.get('/classinfo', (req, res)=>{
+    const params = {}
+    res.status(200).render('classinfo.pug', params);
+})
+
+
+
+
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the DataBase")
+    }).catch(()=>{
+        res.status(400).send("Item was not sent to Db")
+    });
+
+    // res.status(200).render('contact.pug');
+})
+
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
